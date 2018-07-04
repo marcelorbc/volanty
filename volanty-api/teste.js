ca8f156cb98d4338cebbf1528286337f94e415da
Marcelo Bueno <sun.marcelo@gmail.com>
2018-07-04 09:46:55 -0300

    teste mongoose

---
+++ volanty-api/teste.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/volanty');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+var kittySchema = mongoose.Schema({
+    name: String
+  });
+
+  var Kitten = mongoose.model('Kitten', kittySchema);
+
+  var silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
+
+// NOTE: methods must be added to the schema before compiling it with mongoose.model()
+kittySchema.methods.speak = function () {
+    var greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  }
+  
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var fluffy = new Kitten({ name: 'fluffy' });
+//fluffy.speak(); // "Meow name is fluffy"
+
+fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    //fluffy.speak();
+    console.log("greeting");
+  });
+
+  Kitten.find(function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+  })
+
+  
